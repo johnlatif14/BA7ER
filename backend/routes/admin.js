e12eb255b6aa30c0b836c222e9867c54c88053db
johnlatif2026 <youremail@example.com>
2025-07-11 03:34:01 +0300

    أول رفع

--- backend/routes/admin.js
+++
@@ -1,8 +0,0 @@
-const express = require("express");
-const router = express.Router();
-const { sendNotificationEmail } = require("../controllers/adminController");
-const { verifyAdmin } = require("../utils/authMiddleware");
-
-router.post("/notify", verifyAdmin, sendNotificationEmail);
-
-module.exports = router;
--- backend/routes/products.js
+++
@@ -1,9 +0,0 @@
-const express = require("express");
-const router = express.Router();
-const { getProducts, addProduct } = require("../controllers/productController");
-const { verifyAdmin } = require("../utils/authMiddleware");
-
-router.get("/", getProducts);
-router.post("/", verifyAdmin, addProduct);
-
-module.exports = router;
